529a508b1e527cc25a22e6d24e261368bec47b27
Lucas Sousa <lucascsousadev@gmail.com>
2022-03-31 23:11:29 -0300

    teste portifolio

--- js/script.js
+++ js/script.js
@@ -15,19 +15,3 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
-
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval( function (){
-    nextImage();
-}, 3000)
-
-function nextImage(){
-    count++;
-    if(count>4){
-        count=1;
-    }
-
-    document.getElementById("radio"+count).checked = true;
-}
